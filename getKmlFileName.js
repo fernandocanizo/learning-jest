fe3d4ca37aa32e4f804b2da62a82223ab4fb7b57
Fernando L. Canizo <fernandocanizo@gmail.com>
2018-06-11 17:51:03 -0400

    Fix possible throwing condition and throw

--- getKmlFileName.js
+++ getKmlFileName.js
@@ -7,11 +7,11 @@ const path = require('path');
 const getKmlFilename = async (workDir) => {
   const pattern = path.join(workDir, '**/*.kml');
   const files = await globby(pattern);
-  if (files.length > 0) {
+  if (files && files.length > 0) {
     // Return first KML found, if there are others (improbable), ignore them
     return path.basename(files[0]);
   } else {
-    throw Error(`No valid KML file in ${workDir}`);
+    throw new Error(`No valid KML file in ${workDir}`);
   }
 };
 
