f44bb89a4cae5fffad2a165c446b4821640088aa
Fernando L. Canizo <fernandocanizo@gmail.com>
2018-06-11 10:55:12 -0400

    Don't try/catch in getKmlFileName

--- getKmlFileName.js
+++ getKmlFileName.js
@@ -5,17 +5,13 @@ const path = require('path');
 
 
 const getKmlFilename = async (workDir) => {
-  try {
-    const pattern = path.join(workDir, '**/*.kml');
-    const files = await globby(pattern);
-    if (files.length > 0) {
-      // Return first KML found, if there are others (improbable), ignore them
-      return path.basename(files[0]);
-    } else {
-      throw Error(`No valid KML file in ${workDir}`);
-    }
-  } catch (e) {
-    throw Error(e);
+  const pattern = path.join(workDir, '**/*.kml');
+  const files = await globby(pattern);
+  if (files.length > 0) {
+    // Return first KML found, if there are others (improbable), ignore them
+    return path.basename(files[0]);
+  } else {
+    throw Error(`No valid KML file in ${workDir}`);
   }
 };
 
